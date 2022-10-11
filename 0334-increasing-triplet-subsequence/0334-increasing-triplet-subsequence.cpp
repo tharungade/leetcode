cf59daa2cfbf8b8304ab07bbe55d46cf301c797f
Gade Tharun <72391827+tharungade@users.noreply.github.com>
2022-10-11 15:25:12 +0530

    Time: 908 ms (5.05%), Space: 61.6 MB (24.04%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -1,25 +1,34 @@
 class Solution {
-public:
-    bool increasingTriplet(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> leftMin(n);
-        int minInd = 0;
-        for(int i = 0; i < n; i++)
-        {
-            leftMin[i] = minInd;
-            if(nums[i] < nums[minInd])
-                minInd = i;
-        }
-        int maxInd = n-1;
-        for(int i = n-2; i >= 0; i--)
-        {
-            if(nums[leftMin[i]] < nums[i] && nums[maxInd] > nums[i])
-                return true;
-            
-            if(nums[i] > nums[maxInd])
-                maxInd = i;
-        }
-        
-        return false;;
+    public:   
+    bool increasingTriplet(vector<int>& nums) {       
+        int  n = nums.size();        
+        if(n < 3) return false;          
+        int mini = nums[0];      
+        for(int i = 1; i < n-1; i++)     
+        {          
+            if(nums[i] < mini)       
+            {                
+                int temp = nums[i];      
+                nums[i] = 2*nums[i] - mini;
+                mini = temp;       
+            }        
+        }  
+        for(auto & i : nums) cout << i << " "; 
+        cout << endl;
+        int maxi = nums[n-1];     
+        for(int i = n-2; i >= 0; i--)        
+        {           
+            if(nums[i] < mini)  
+            {               
+                maxi = max(maxi, mini);     
+                mini = 2*mini - nums[i];      
+            }           
+            else{         
+                maxi = max(maxi, nums[i]);   
+                if(mini < nums[i] && maxi > nums[i])      
+                    return true;         
+            }       
+        }            
+        return false;  
     }
 };
