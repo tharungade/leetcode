49d9baa21fa94be940207c09e3ff4323979cf263
Gade Tharun <72391827+tharungade@users.noreply.github.com>
2022-10-20 01:23:31 +0530

    Time: 36 ms (16.00%), Space: 12.5 MB (88.47%) - LeetHub

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -0,0 +1,35 @@
+class myComparator
+{
+public:
+    int operator() (const pair<int,string> &c1,const pair<int,string> &c2)
+    {
+        if(c1.first == c2.first)
+            return c1.second < c2.second;
+        return c1.first > c2.first;
+    }
+};
+class Solution {
+public:
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        vector<string> res;
+        priority_queue< pair<int,string>, vector<pair<int,string>>, myComparator > minHeap;
+        
+        map<string, int> mp;
+        for(auto & i : words) mp[i]++;
+        
+       for(auto & i : mp)
+       {
+           minHeap.push({i.second, i.first});
+           if(minHeap.size() > k)
+               minHeap.pop();
+       }
+        
+        while(minHeap.size() > 0)
+        {
+            res.insert(res.begin(), minHeap.top().second);
+            minHeap.pop();
+        }
+        
+        return res;
+    }
+};
