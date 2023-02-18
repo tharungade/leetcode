8689c237560071149867c7197838cb4a59e26b95
Gade Tharun <72391827+tharungade@users.noreply.github.com>
2023-02-18 16:46:31 +0530

    Added solution - LeetHub

---
+++ Apple Sequences - GFG/apple-sequences.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{   
+public:
+    int appleSequences(int n, int m, string arr){
+        // code here 
+        int leftPtr = -1, res = 1;
+        for(int i = 0; i < n; ++i)
+        {
+           if(arr[i] == 'O')
+           {
+               if(m <= 0)
+                while(arr[++leftPtr] != 'O');
+               else
+                m--;
+           }
+           res = max(res, i-leftPtr);
+        }
+        
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N, M;
+        cin >> N >> M;
+        string arr;
+        cin >> arr;
+        
+        Solution ob;
+        cout << ob.appleSequences(N, M, arr) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
